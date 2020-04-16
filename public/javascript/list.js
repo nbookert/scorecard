92866ef95e70ec5400f43b83dca202d18cfba641
Nyteisha Bookert <CIO@My-MacBook-Pro.local>
2020-04-16 15:00:40 -0400

    removed unnecessary code

--- public/javascript/list.js
+++ public/javascript/list.js
@@ -3,22 +3,14 @@ function show_list(){
     d3.csv("data/hbcus-list.csv").then(function(data){
         data.forEach(function(d) {
             d.years=+d.years
-            d.place = d.place
-            d.lat=parseFloat(d.lat)
-            d.lon=parseFloat(d.lon)
-
-        });
-
-        sdata=data;
-        for(var i=0;i<sdata.length;i++){
-            if(sdata[i].years == 2){
-                d3.select("#list-div")
+            if(d.years==2){
+                 d3.select("#list-div")
                 .append("p")
                 .append("text")
                 .style("font-size","small")
-                .text(sdata[i].place)
+                .text(d.place)
             }
-        }
+        });
     }).catch(function(error){
         console.log(error);
     });
