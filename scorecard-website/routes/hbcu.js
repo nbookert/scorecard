937063c7ec05b1ead15bde9933f07c04d2e531da
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2020-04-26 14:48:44 -0400

    Updated MongoDB to remote source

--- scorecard-website/routes/hbcu.js
+++ scorecard-website/routes/hbcu.js
@@ -35,9 +35,9 @@ router.get('/scores', function(req, res, next) {
 });
 
 router.get('/institution-data', function(req,res, next) {
-  MongoClient.connect("mongodb://localhost", function(err, client){
-     let db = client.db("finalproject");
-     db.collection('institution1819', function(err, collection) {
+  MongoClient.connect("mongodb+srv://comp895va:dnjn895@hbcu-scoreboard-sjn7a.mongodb.net", function(err, client){
+     let db = client.db("scorecard");
+     db.collection('academicyear20182019', function(err, collection) {
         collection.find({HBCU:"1"},{projection: {ADM_RATE: 1, ACTCMMID: 1, UGDS: 1, LATITUDE: 1, LONGITUDE: 1, C150_4: 1, TUITIONFEE_IN: 1,
             INSTNM: 1, TUITIONFEE_OUT: 1, CONTROL: 1, SAT_AVG: 1, INSTURL: 1}}).toArray(function(err, result) {
             if (err) {
